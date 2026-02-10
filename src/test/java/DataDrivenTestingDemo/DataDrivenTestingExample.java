5f6eb88b34e60024bf1903361c92d65daa7ff308
rafiehayat <rafiehayat5@gmail.com>
2026-02-10 21:06:14 +0530

    To Red Data From Excel

---
+++ src/test/java/DataDrivenTestingDemo/DataDrivenTestingExample.java
@@ -0,0 +1,72 @@
+package DataDrivenTestingDemo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class DataDrivenTestingExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		XSSFWorkbook ExcelWBook = null;
+		XSSFSheet ExcelWSheet;
+//		XSSFRow Row;
+//		XSSFCell Cell;
+		
+		//Create an object of File Class to Open File
+		File excelFile = new File("/Users/rafiehayat/Desktop/Testing.xlsx");
+		FileInputStream inputStream = null;
+		
+		//Create an object of FileInputStread Class to Read File
+		try {
+			 inputStream = new FileInputStream(excelFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// Excel -> Workbook -> Sheet -> Row -> Cell
+		//Create object of XSSFWorkbook to handle xlsx file
+		try {
+			ExcelWBook = new XSSFWorkbook(inputStream);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//To Access Workbook Sheet
+		ExcelWSheet = ExcelWBook.getSheetAt(0);
+		
+		//Get Total Row Count
+		int ttlRows = ExcelWSheet.getLastRowNum() + 1;
+		
+		// Get total no. of cells in a row
+		int ttlCells = ExcelWSheet.getRow(0).getLastCellNum();
+		
+		for(int currentRow = 0; currentRow < ttlRows; currentRow ++ )
+		{
+			for(int currentCell = 0; currentCell < ttlCells;  currentCell ++)
+			{
+				System.out.print(ExcelWSheet.getRow(currentRow).getCell(currentCell).toString());
+				System.out.print("\t");
+			}
+			System.out.print("\n");
+
+		}
+		
+		try {
+			ExcelWBook.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	}
+
+}
