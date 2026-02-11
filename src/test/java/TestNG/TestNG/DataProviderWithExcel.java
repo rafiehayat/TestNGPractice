f5852e1187cc77aaadee54fc87b7d43b6664d231
rafiehayat <rafiehayat5@gmail.com>
2026-02-11 21:30:49 +0530

    TestNG Data Provider By Reading Excel File

--- src/test/java/TestNG/TestNG/DataProviderWithExcel.java
+++ src/test/java/TestNG/TestNG/DataProviderWithExcel.java
@@ -2,6 +2,12 @@ package TestNG.TestNG;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -32,12 +38,45 @@ public class DataProviderWithExcel {
 	@DataProvider(name = "searchDataProvider")
 	public Object[][] searchDataProviderMethod()
 	{
-		Object [][] searchData = new Object [2][1];
-		searchData [0][0] = "Taj Mahal";
-		searchData [1][0] = "India Gate";
+		String fileName = "/Users/rafiehayat/Desktop/SearchPOI.xlsx";
+		Object [][] searchData = getExcelData(fileName,"Sheet1");
+//		Object [][] searchData = new Object [2][1];
+//		searchData [0][0] = "Taj Mahal";
+//		searchData [1][0] = "India Gate";
 		return  searchData;
+	}
+	public String [][] getExcelData(String filename,String sheetName)
+	{
+		String [][] data = null;
+		try {
+			FileInputStream inputStream = new FileInputStream(filename);
+			XSSFWorkbook workBook = new XSSFWorkbook(inputStream);
+			XSSFSheet excelSheet =  workBook.getSheet(sheetName);
+			
+			//get Total no. of Rows
+			int ttlRows =  excelSheet.getLastRowNum() + 1;
+			
+			//get Total no. of Cells
+			int ttlCells = excelSheet.getRow(0).getLastCellNum();
+			
+			// Initialize Array
+			data = new String [ttlRows - 1 ] [ttlCells];
+			
+			for(int currentRow = 1; currentRow< ttlRows;currentRow++)  // loop for row
+			{
+				for(int currentCell = 0;currentCell< ttlCells;currentCell++)
+				{
+					data [currentRow - 1][currentCell] = excelSheet.getRow(currentRow).getCell(currentCell).getStringCellValue();
+				}
+			}
 
-		
+			
+			workBook.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return data;
 	}
 	@AfterMethod
 	public void tearDown()
