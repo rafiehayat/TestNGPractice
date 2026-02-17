ed671b0917ab3b5df37a09fca579ec26bb73e8be
rafiehayat <rafiehayat5@gmail.com>
2026-02-17 11:18:16 +0530

    Using Reporter Class

--- src/test/java/TestNGReportDemo/TestNGReportEx2.java
+++ src/test/java/TestNGReportDemo/TestNGReportEx2.java
@@ -1,4 +1,5 @@
 package TestNGReportDemo;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -13,11 +14,15 @@ public class TestNGReportEx2 {
 	{
 		WebDriver cd = new ChromeDriver();
 		cd.manage().window().maximize();
+		Reporter.log("Chrome broowser Opened");
 		cd.get("https://www.google.com/");
+		Reporter.log("URL Opened");
+		
 		
 		WebElement  searchBox = cd.findElement(By.name("q"));
 		
 		searchBox.sendKeys("India Gate");
+		Reporter.log("India Gate Keyword");
 		searchBox.sendKeys(Keys.ENTER);
 		
 		
