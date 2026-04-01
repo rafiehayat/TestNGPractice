f931ea943b6fe47f6056894e93e0def7b54a5297
rafiehayat <rafiehayat5@gmail.com>
2026-04-01 21:03:22 +0530

    Revision1

---
+++ src/test/java/BasicTestCase/BasicTestCase.java
@@ -0,0 +1,28 @@
+package BasicTestCase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicTestCase {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver cd = new ChromeDriver();
+		cd.manage().window().maximize();
+		cd.get("https://www.saucedemo.com/");
+		
+		cd.findElement(By.id("user-name")).sendKeys("standard_user");
+		cd.findElement(By.id("password")).sendKeys("secret_sauce");
+		cd.findElement(By.id("login-button")).click();
+		
+		String currentWindowhandle = cd.getWindowHandle();
+		cd.switchTo().window(currentWindowhandle);
+		cd.findElement(By.linkText("Sauce Labs Backpack")).click();
+		
+		Thread.sleep(30000);
+		cd.quit();
+
+	}
+
+}
