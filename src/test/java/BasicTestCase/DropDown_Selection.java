d2f3e5db6d0e42678da75d7b0047eeaf5ab407eb
rafiehayat <rafiehayat5@gmail.com>
2026-04-05 11:29:48 +0530

    Dropdown

---
+++ src/test/java/BasicTestCase/DropDown_Selection.java
@@ -0,0 +1,27 @@
+package BasicTestCase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropDown_Selection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver cd = new ChromeDriver();
+		cd.manage().window().maximize();
+		cd.get("https://www.geodatasource.com/software/country-region-dropdown-menu-demo");
+		
+		WebElement element = cd.findElement(By.className("form-select"));
+		
+		Select dropdown = new Select(element);
+//		dropdown.selectByVisibleText("Afghanistan");
+//		dropdown.selectByValue("Afghanistan");
+		dropdown.selectByIndex(1);
+
+
+	}
+
+}
