70cb7f6b9ffdb3ff845117e4681e623652b1ac35
rafiehayat <rafiehayat5@gmail.com>
2026-04-08 22:03:07 +0530

    FlipkartPopupClose

---
+++ src/test/java/BasicTestCase/FlipkartPopupClose.java
@@ -0,0 +1,29 @@
+package BasicTestCase;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class FlipkartPopupClose {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 ChromeDriver cd = new ChromeDriver();
+	        cd.manage().window().maximize();
+	        cd.get("https://www.flipkart.com/");
+	        
+//	        WebDriverWait wait = new WebDriverWait(cd, Duration.ofSeconds(10));
+	        
+	        try {
+	        	Thread.sleep(1000); 
+	            
+	            Actions action = new Actions(cd);
+	            action.sendKeys(Keys.ESCAPE).build().perform();
+	            
+	            System.out.println("Sent Escape key to close popup.");
+	        } catch (Exception e) {
+	            System.out.println("Popup did not appear or could not be closed: " + e.getMessage());
+	        }
+
+	}
+
+}
