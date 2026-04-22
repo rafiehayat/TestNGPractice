b41fb75e5cf8f8631784ef5f86f74bbef913b687
rafiehayat <rafiehayat5@gmail.com>
2026-04-22 21:02:05 +0530

    Encapsulation

---
+++ src/test/java/LoopJava/EncapsulationDemo.java
@@ -0,0 +1,18 @@
+package LoopJava;
+
+public class EncapsulationDemo {
+	private int accno;
+	private String name;
+	private double amount;
+	
+	
+	void setaccno (int accno)
+	{
+		this.accno = accno;
+	}
+	
+	int getaccno ()
+	{
+		return accno;
+	}
+}
---
+++ src/test/java/LoopJava/EncapsulationDemoMain.java
@@ -0,0 +1,12 @@
+package LoopJava;
+
+public class EncapsulationDemoMain {
+
+	public static void main(String[] args) {
+		EncapsulationDemo acc = new EncapsulationDemo();
+		
+		acc.setaccno(101);
+		System.out.println(acc.getaccno());
+	}
+
+}
