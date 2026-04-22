4b344aa73e5f0cfecaae3a9591636ad64a5df972
rafiehayat <rafiehayat5@gmail.com>
2026-04-22 20:21:49 +0530

    Polymorphism

---
+++ src/test/java/LoopJava/PolymorphismDemoMain.java
@@ -0,0 +1,12 @@
+package LoopJava;
+
+public class PolymorphismDemoMain {
+
+	public static void main(String[] args) {
+		PolymorphismDemo poly = new PolymorphismDemo();
+		
+		poly.sum();
+		poly.sum(10, 20);
+	}
+
+}
