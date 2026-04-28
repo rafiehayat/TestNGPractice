41051bda4b4d7b0f04e11842da659ae5d9f10719
rafiehayat <rafiehayat5@gmail.com>
2026-04-28 21:04:41 +0530

    TypecastingDemo2

--- src/test/java/LoopJava/TypecastingDemo.java
+++ src/test/java/LoopJava/TypecastingDemo.java
@@ -8,7 +8,8 @@ class Cat extends Animal{}
 public class TypecastingDemo {
 
 	public static void main(String[] args) {
-		
+		// Cat ct = (Cat) an;
+		// A ,  B ,   C ,  D
 		// Rule 1 - conversion is valid or not 
 		//the type of 'd' and 'c' must have relationship (either parent to child or child to parent)
 		Animal an = new Dog();
@@ -22,8 +23,22 @@ public class TypecastingDemo {
 		// Rule 2 - Assignment is valid or not
 		// 'c' must be same or child of 'a'
 		
-		Animal an3 = new Dog();
-		Cat ct3 = (Cat) an3;
+//		Animal an3 = new Dog();
+//		Cat ct3 = (Cat) an3;    // valid as per rule 2
+//		
+//		Animal an4 = new Dog();
+//		Cat ct4 = (Dog) an4;    // Invalid as per rule 2 
+		
+		//rule3
+		// the underlying object type of 'd' must be either same or child of 'c'
+		
+//		Animal an5 = new Dog();
+//		Cat ct5 = (Cat) an;      // invalid as per rule 3
+		
+		// rule1 , rule 2 ,rule3 
+		Animal an6 = new Dog();
+		Dog dg2 = (Dog) an;     // rule1 - yes    rule2 - yes  rule3 - yes
+
 		
 	}
 
