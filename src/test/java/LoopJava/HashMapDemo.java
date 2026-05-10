ecb59da7d29ac1e92da23546c385f54893f98858
rafiehayat <rafiehayat5@gmail.com>
2026-05-10 11:23:18 +0530

    HashMapDemo

--- src/test/java/LoopJava/HashMapDemo.java
+++ src/test/java/LoopJava/HashMapDemo.java
@@ -1,7 +1,9 @@
 package LoopJava;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class HashMapDemo {
 
@@ -36,6 +38,36 @@ public class HashMapDemo {
 		
 		//access the value of the key
 		System.out.println(hm3.get(101));
+		
+		//get all the keys from hashmap
+		System.out.println(hm3.keySet());
+		
+		//get all the values key and set both
+		System.out.println(hm3.entrySet());
+		
+		
+		//Reading Data From hashMap
+		//Using For each
+		
+		for(int i:hm3.keySet())
+		{
+			System.out.println(i +"      " + hm3.get(i));
+		}
+		
+		//Using Iterator
+		Iterator<Entry<Integer, String>> it = hm3.entrySet().iterator();
+		
+		while(it.hasNext())
+		{
+//			System.out.println(it.next());
+			Entry<Integer, String> entry = it.next();
+			System.out.println(entry.getKey()+"--   " + entry.getValue());
+		}
+//		mySet.clear();
+//		System.out.println("After removing Element - " +mySet); 
+		
+
+
 
 
 
